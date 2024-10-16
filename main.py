46cf98487c72a10fdd72d9e2a0fd63b8438e4dc0
pouyannc <pouyannc@gmail.com>
2024-10-16 18:21:20 -0400

    rotate player left and right

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -18,11 +19,13 @@ def main():
                 return
 
         screen.fill("black")
+        player1.draw(screen)
+        player1.update(dt)
+
         pygame.display.flip()
 
         t = clock.tick(60)
         dt = t / 1000
-        print(dt)
 
 if __name__ == "__main__":
     main()
