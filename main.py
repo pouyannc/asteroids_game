e652a4e2e505144aad410ca443304c66300649d4
pouyannc <pouyannc@gmail.com>
2024-10-16 17:52:54 -0400

    limit frames to 60

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(color="black")
+        screen.fill("black")
         pygame.display.flip()
+
+        t = clock.tick(60)
+        dt = t / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
