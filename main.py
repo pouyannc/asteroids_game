c37cf1814bddf5e8004a4ff9ae3ed81ee404a556
pouyannc <pouyannc@gmail.com>
2024-10-17 15:17:07 -0400

    shot class for shooting circles from player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,10 +17,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, drawable, updatable)
     
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -39,7 +42,6 @@ def main():
             if obj.check_collision(player1):
                 print("Game over!")
                 return
-
 
         pygame.display.flip()
 
